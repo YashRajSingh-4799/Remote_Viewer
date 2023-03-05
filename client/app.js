dd4149b1fde36eded51940c28fdc070e1db0e768
Yash Raj Singh <yashrajsingh4799@gmail.com>
2023-03-05 11:06:03 +0530

    screensharing function completed

--- client/app.js
+++ client/app.js
@@ -1,42 +1,54 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const path = require('path')
-const {v4: uuidv4} = require('uuid');
-const screenshot= require('screenshot-desktop');
-var socket= require('socket.io-client')('http://localhost:5000')
+const { app, BrowserWindow, ipcMain } = require("electron");
+const { v4: uuidv4 } = require("uuid");
+const screenshot = require("screenshot-desktop");
 
+var socket = require("socket.io-client")("http://localhost:5000");
+var interval;
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
-    width: 600,
-    height: 150,
+    width: 500,
+    height: 200,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
-
+      nodeIntegration: true,
+    },
+  });
   win.removeMenu();
-  win.loadFile('index.html')
+  win.loadFile("index.html");
 }
-app.whenReady().then(() => {
-  createWindow()
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
+app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-ipcMain.on("start-share", function(event, arg){
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
 
-})
+ipcMain.on("start-share", function (event, arg) {
+  var uuid = uuidv4();
+  socket.emit("join-message", uuid);
+  event.reply("uuid", uuid);
 
-ipcMain.on("stop-share", function(event, arg){           
+  interval = setInterval(function () {
+    screenshot().then((img) => {
+      var imgStr = new Buffer(img).toString("base64");
 
-})
+      var obj = {};
+      obj.room = uuid;
+      obj.image = imgStr;
+
+      socket.emit("screen-data", JSON.stringify(obj));
+    });
+  }, 100);
+});
+
+ipcMain.on("stop-share", function (event, arg) {
+  clearInterval(interval);
+});
--- client/index.js
+++ client/index.js
@@ -1,18 +1,19 @@
-const ipcRenderer = require('electron').ipcRenderer;
+const ipcRenderer = Require("electron").ipcRenderer;
 
-window.onload = function(){
+window.onload = function () {
+  ipcRenderer.on("uuid", (event, data) => {
+    document.getElementById("code").innerHTML = data;
+  });
+};
 
+function startShare() {
+  ipcRenderer.send("start-share", {});
+  document.getElementById("start").style.display = "none";
+  document.getElementById("stop").style.display = "block";
 }
 
-function startShare(){
-    ipcRenderer.send('start-share',{});
-    document.getElementById("start").style.display="none";
-    document.getElementById("stop").style.display="block";
-
-}
-
-function stopShare(){
-    ipcRenderer.send('stop-share',{});
-    document.getElementById("stop").style.display="none";
-    document.getElementById("start").style.display="block";
+function stopShare() {
+  ipcRenderer.send("stop-share", {});
+  document.getElementById("stop").style.display = "none";
+  document.getElementById("start").style.display = "block";
 }
